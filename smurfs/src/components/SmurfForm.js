ca66ddbee05b2f7565bc60cb96b591d523acf178
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-08-16 10:15:37 -0600

    build structure for SmurfForm

--- smurfs/src/components/SmurfForm.js
+++ smurfs/src/components/SmurfForm.js
@@ -3,7 +3,12 @@ import React from "react";
 const SmurfForm = props => {
     return (
         <div>
-            <h2>From SmurfForm to App</h2>
+            <form>
+                <input type="text" placeholder='Smurf Name'/>
+                <input type="text" placeholder='Smurf Age'/>
+                <input type="text" placeholder='Smurf Height'/>
+            </form>
+            <button>Add Smurf</button>
         </div>
     )
 }
