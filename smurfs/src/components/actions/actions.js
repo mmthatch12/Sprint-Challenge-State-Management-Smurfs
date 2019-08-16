ebdd352b6d00570b1fa9af120c8b58dbd79fe230
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-08-16 09:50:40 -0600

    still working on actions.js

--- smurfs/src/components/actions/actions.js
+++ smurfs/src/components/actions/actions.js
@@ -1,5 +1,10 @@
 import axios from 'axios'
 
 export const getSmurfs = () => {
-    
+    return dispatch => {
+        axios
+            .get('http://localhost:3333/smurfs')
+            .then(res => console.log(res))
+            .catch(err => console.log('error'))
+    }
 }
