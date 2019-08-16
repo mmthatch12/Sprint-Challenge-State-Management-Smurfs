cbb7a202492b4176a2b706c1660ee57e00c5dbd5
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-08-16 14:54:07 -0600

    redoing my form with formic

--- smurfs/src/components/SmurfForm.js
+++ smurfs/src/components/SmurfForm.js
@@ -1,37 +1,53 @@
-import React, { useState } from "react";
+import React from "react";
 import { connect } from "react-redux";
+import { Form, Field, withFormik } from 'formik'
+import axios from 'axios'
 
 
-import { postSmurf } from './actions/actions'
 
-const SmurfForm = props => {
-    const[newSmurf, setNewSmurf] = useState([{ name: '', age: 0, height: '', id: 0 }])
+const SmurfForm = () => {
+    // const[newSmurf, setNewSmurf] = useState({ name: '', age: 0, height: '' })
     
-    const handleChange = event => {
-        event.preventDefault();
-        setNewSmurf({
-            ...newSmurf, [event.target.name]: event.target.value
-        })
-    }
+    // const handleChange = event => {
+    //     event.preventDefault();
+    //     setNewSmurf({
+    //         ...newSmurf, [event.target.name]: event.target.value
+    //     })
+    // }
 
-    const addSmurf = event => {
-        event.preventDefault()
-        props.postSmurf(newSmurf)
-        setNewSmurf({ name: '', age: 0, height: ''})
-    }
+    // const addSmurf = event => {
+    //     event.preventDefault()
+    //     console.log("newsmurf", newSmurf)
+    //     props.postSmurf(newSmurf)
+    //     setNewSmurf({ name: '', age: 0, height: ''})
+    // }
     
 
     return (
         <div>
-            <form onSubmit={() => console.log(newSmurf)}>
+            <Form>
+                <Field type='text' name='name' placeholder='Smurf Name' />
+                <Field type='number' name='age' placeholder='Smurf Age' />
+                <Field type='text' name='height' placeholder='Smurf Height' />
+                <button type='submit'>Add Smurf!</button>
+            </Form>
+            {/* <form onSubmit={addSmurf}>
                 <input type="text" value={newSmurf.name} name='name' onChange={handleChange} placeholder='Smurf Name'  />
                 <input type="number" value={newSmurf.age} name='age' onChange={handleChange} placeholder='Smurf Age' />
                 <input type="text" value={newSmurf.height} name='height' onChange={handleChange} placeholder='Smurf Height'  />
             </form>
-            <button onClick={props.postSmurf}>Add Smurf</button>
+            <button onClick={props.postSmurf}>Add Smurf</button> */}
         </div>
     )
 }
 
-export default connect(null, { postSmurf })(SmurfForm)
+const FormicSmurfForm = withFormik({
+    mapPropsToValues({ name, age, height }) {
+        return {
+
+        }
+    }
+})
+
+export default SmurfForm
 
--- smurfs/src/components/actions/actions.js
+++ smurfs/src/components/actions/actions.js
@@ -1,9 +1,11 @@
 import axios from 'axios'
 
+
+
 export const GET_SMURF_DATA_SUCCESS = 'GET_SMURF_DATA_SUCCESS'
 export const GET_SMURF_DATA_FAILURE = 'GET_SMURF_DATA_FAILURE'
-export const POST_SMURF_SUCCESS = 'POST_SMURF_SUCCESS'
-export const POST_SMURF_FAILURE = 'POST_SMURF_FAILURE'
+// export const POST_SMURF_SUCCESS = 'POST_SMURF_SUCCESS'
+// export const POST_SMURF_FAILURE = 'POST_SMURF_FAILURE'
 
 
 export const getSmurfs = () => {
@@ -18,19 +20,19 @@ export const getSmurfs = () => {
     }
 }
 
-export const postSmurf = values => {
-    return dispatch => {
-        axios
-            .post('http://localhost:3333/smurfs', values)
-            .then(res => {
-                console.log('post request', res.data)
-                dispatch({ type: POST_SMURF_SUCCESS, payload: res.data})
-            })
-            .catch(err => {
+// export const postSmurf = values => {
+//     return dispatch => {
+//         axios
+//             .post('http://localhost:3333/smurfs', values)
+//             .then(res => {
+//                 console.log('post request', res.data)
+//                 dispatch({ type: POST_SMURF_SUCCESS, payload: res.data})
+//             })
+//             .catch(err => {
                 
-                console.log('error', err.response)
-                dispatch({ type: POST_SMURF_FAILURE, payload: err.response })
-            })
+//                 console.log('error', err.response)
+//                 dispatch({ type: POST_SMURF_FAILURE, payload: err.response })
+//             })
 
-    }
-}
+//     }
+// }
