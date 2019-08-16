bf4b540aa8b13b4dcb0f1bc3e5c7c3a09b70f900
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-08-16 10:36:41 -0600

    still working on SmurfForm

--- smurfs/src/components/SmurfForm.js
+++ smurfs/src/components/SmurfForm.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+
+
 
 const SmurfForm = props => {
+    
+    const [newSmurf, setNewSmurf] = useState([{ name: '', age: 0, height: '', id: 0 }])
+
+    const handleChanges = event => {
+        setNewSmurf({...newSmurf,
+            [event.target.name]: event.target.value
+        })
+    }
+
+    const submitSmurf = event => {
+        event.preventDefault()
+
+    }
+
+    const addSmurf
+
+
     return (
         <div>
-            <form>
-                <input type="text" placeholder='Smurf Name'/>
-                <input type="text" placeholder='Smurf Age'/>
-                <input type="text" placeholder='Smurf Height'/>
+            <form onSubmit={submitSmurf} >
+                <input type="text" value={newSmurf.name} name='name' placeholder='Smurf Name' onChange={handleChanges} />
+                <input type="text" value={newSmurf.age} name='age' placeholder='Smurf Age'onChange={handleChanges} />
+                <input type="text" value={newSmurf.height} name='height' placeholder='Smurf Height' onChange={handleChanges} />
             </form>
             <button>Add Smurf</button>
         </div>
