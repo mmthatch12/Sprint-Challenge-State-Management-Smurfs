0a51e555588d88df637f80ff7a1ccc130437b018
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-08-16 09:43:54 -0600

    working on SmurfList.js

--- smurfs/src/components/SmurfList.js
+++ smurfs/src/components/SmurfList.js
@@ -1,11 +1,23 @@
 import React from "react";
+import { connect } from "react-redux";
 
 import Smurf from './Smurf'
 
 const SmurfList = props => {
     return (
-        <Smurf /> 
+        <div>
+            <Smurf /> 
+        </div>
     )
 }
 
-export default SmurfList
+const mapStateToProps = state => {
+    return {
+        smurfs: state.smurfs,
+
+    }
+}
+
+export default connect(
+    mapStateToProps, {}
+)(SmurfList)
