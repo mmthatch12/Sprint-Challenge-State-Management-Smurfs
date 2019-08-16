71dcb3c0e13b794e92c1c8d44420d0d66246a9db
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-08-16 10:11:38 -0600

    got my api data to load when get smurfs is clicked!

--- smurfs/src/components/Smurf.js
+++ smurfs/src/components/Smurf.js
@@ -3,9 +3,9 @@ import React from "react";
 const Smurf = props => {
     return (
         <div>
-            <h2>{props.name}</h2>
-            <h2>{props.age}</h2>
-            <h2>{props.height}</h2>
+            <h2>{props.smurfData.name}</h2>
+            <h2>{props.smurfData.age}</h2>
+            <h2>{props.smurfData.height}</h2>
         </div>
     )
 }
