d1b0aea47d21f06d81a24f051b68787721c3a894
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-08-16 09:33:08 -0600

    built default reducer

--- smurfs/src/components/reducers/reducers.js
+++ smurfs/src/components/reducers/reducers.js
@@ -1,3 +1,11 @@
 const initialState = {
-    
+    smurfs: [],
+    test: 'this is a test'
+}
+
+export const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
 }
